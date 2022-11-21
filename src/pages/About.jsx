9452bec0d68463c2212397ceaac6cd7c057cede7
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-11-21 19:51:58 +0330

    add About text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,27 @@
 import React from "react"
 
-export default function About(){
+export default function About() {
     document.getElementById("title").innerHTML = "About"
     return (
-        <h1>About</h1>
+        <div className="aboutContainer">
+            <p>
+                Hi, I’m Mohammad Hosein!
+            </p>
+            <p>
+                I'm a web developer and a computer engineering student.
+            </p>
+            <p>
+                In a nutshell, I create front-end of websites.
+            </p>
+            <p>
+                In 2019, I entered the field of computer engineering at Yazd University
+            </p>
+            <p>
+                My expertise lies within front-end web apps, and the main languages in my tech stack are React, JavaScript, and of course HTML/CSS.
+            </p>
+            <p>
+                I’m a lifelong learner and love to code, read, swim .
+            </p>
+        </div>
     )
 }
