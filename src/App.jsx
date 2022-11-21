a4015997b965053021d411237bb4a8cb35964f6f
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-11-21 18:04:34 +0330

    add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,23 @@ import Projects from "./pages/Projects"
 import Skills from "./pages/Skills"
 import NotFound from "./pages/NotFound"
 import Navbar from "./components/Navbar"
+import './styles/main.css'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Navbar />}>
-          <Route index path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Projects" element={<Projects />} />
-          <Route path="/Skills" element={<Skills />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="appContainer">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Navbar />}>
+            <Route index path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Skills" element={<Skills />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react"
+import '../styles/home.css'
 
 export default function Home(){
     document.getElementById("title").innerHTML = "Home"
     return (
-        <h1>Home</h1>
+        <div className="homeContainer">
+            <h3>Hey, I'm</h3>
+            <h1>Mohammad Hosein Zamani</h1>
+        </div>
     )
 }
