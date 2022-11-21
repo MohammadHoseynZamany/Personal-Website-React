965ea66145c1540cec4310ce2956330a1f4b27dd
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-11-21 21:42:23 +0330

    add Skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,35 @@
 import React from "react"
+import ReactLogo from "../images/reactLogo.svg"
+import JavaScriptLogo from "../images/javascriptLogo.svg"
+import HtmlLogo from "../images/htmlLogo.svg"
+import CssLogo from "../images/cssLogo.svg"
+import PythonLogo from "../images/pythonLogo.svg"
+import "../styles/skills.css"
 
 export default function Skills(){
     document.getElementById("title").innerHTML = "Skills"
     return (
-        <h1>Skills</h1>
+        <div className="skillsContainer">
+            <div className="reactContainer">
+                <img src={ ReactLogo } />
+                <p>React</p>
+            </div>
+            <div className="javaScriptContainer">
+                <img src={ JavaScriptLogo } />
+                <p>JavaScript</p>
+            </div>
+            <div className="htmlContainer">
+                <img src={ HtmlLogo } />
+                <p>Html</p>
+            </div>
+            <div className="cssContainer">
+                <img src={ CssLogo } />
+                <p>Css</p>
+            </div>
+            <div className="pythonContainer">
+                <img src={ PythonLogo } />
+                <p>Python</p>
+            </div>
+        </div>
     )
 }
