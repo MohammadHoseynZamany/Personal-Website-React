e9e06e05deae4e35dc2893f0b2ff2df3f0247a68
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-11-24 09:41:25 +0330

    add text writer at home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,35 @@ import '../styles/home.css'
 
 export default function Home(){
     document.getElementById("title").innerHTML = "Home"
+
+    let i = 0;
+    const txtFirst = "Hi, I'm";
+    const txtSecond = "          Mohammad Hosein Zamani"
+    const speed = 200;
+
+    // document.getElementsByClassName("homeContainer")[0].addEventListener("load", typeWriter)
+    React.useEffect(()=>{
+        window.onload = typeWriter()
+    }, [])
+
+    function typeWriter() {
+        if (i < txtFirst.length) {
+            document.getElementById("homeFirstText").innerHTML += txtFirst.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+            console.log(i)
+        }
+        else if (i < txtFirst.length + txtSecond.length){
+            document.getElementById("homeSecondText").innerHTML += txtSecond.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed*1.5);
+        }
+    }
+
     return (
         <div className="homeContainer">
-            <h3>Hey, I'm</h3>
-            <h1>Mohammad Hosein Zamani</h1>
+            <h2 id="homeFirstText"></h2>
+            <h1 id="homeSecondText"></h1>
         </div>
     )
 }
